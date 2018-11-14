7d4bfc20f2ec3737a94e98ba34d36308692fde69
BuildTools <unconfigured@null.spigotmc.org>
2018-11-14 22:57:35 +0100

    update

---
+++ src/main/java/com/morkaz/morkazsk/MorkazSk.java
@@ -0,0 +1,18 @@
+package com.morkaz.morkazsk;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MorkazSk extends JavaPlugin {
+
+	@Override
+	public void onEnable(){
+
+	}
+
+	@Override
+	public void onDisable(){
+		
+	}
+
+
+}
